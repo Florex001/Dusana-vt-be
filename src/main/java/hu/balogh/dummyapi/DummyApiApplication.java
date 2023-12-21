82c81874c8b405159ccf07237959e3206a3d34ee
Balogh Dávid <david.balogh.2002@gmail.com>
2023-12-21 10:48:33 +0100

    swagger-ui

--- src/main/java/hu/balogh/dummyapi/DummyApiApplication.java
+++ src/main/java/hu/balogh/dummyapi/DummyApiApplication.java
@@ -1,13 +1,35 @@
 package hu.balogh.dummyapi;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DummyApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DummyApiApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI openAPI(){
+		Info info = new Info()
+				.title("Kosuth vadásztársaság")
+				.description("Kosuth Vadásztársaság API.")
+				.license(new License()
+						.name("Apache 2.0")
+						.url("https://www.apache.org/licenses/LICENSE-2.0"))
+				.contact(new Contact()
+						.name("Balogh Dávid")
+						.url("https://baloghjavadev.com/")
+						.email("balogh.david.web@gmail.com")
+				);
+
+		return new OpenAPI().info(info);
 	}
 
 }
