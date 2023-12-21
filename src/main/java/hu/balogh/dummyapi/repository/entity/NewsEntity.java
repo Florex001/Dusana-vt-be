45389386da1b9faa77b3d9c190cf8acc503a15e5
Balogh Dávid <david.balogh.2002@gmail.com>
2023-12-21 16:44:28 +0100

    mysql

--- src/main/java/hu/balogh/dummyapi/repository/entity/NewsEntity.java
+++ src/main/java/hu/balogh/dummyapi/repository/entity/NewsEntity.java
@@ -1,20 +1,22 @@
 package hu.balogh.dummyapi.repository.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
 @Entity
-@Table(name = "News")
+@Table(name = "news")
 public class NewsEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String temacim;
-    private LocalDateTime temadatum;
+    private LocalDate temadatum;
     private String temaleiras;
 
 }
