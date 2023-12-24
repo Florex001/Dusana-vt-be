a173ee323846013d72c6d51f0a05cb82c9a14576
Balogh Dávid <david.balogh.2002@gmail.com>
2023-12-24 11:02:03 +0100

    merry x-mas

--- src/main/java/hu/balogh/dusanaVT/Application.java
+++ src/main/java/hu/balogh/dusanaVT/Application.java
@@ -8,13 +8,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableScheduling
+@RequestMapping
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/")
+	public String meryXmas(){
+		return "<h1>Boldog Karácsonyt Törci!!!!</h1>";
 	}
 
 }
